8063cc6d2ace3d3cee5adabfb12a7129048ec612
naufalfidyanramadhan <54935087+naufalfidyanramadhan@users.noreply.github.com>
2020-10-21 11:55:40 -0700

    navigation 3

--- app/src/main/java/id/ac/polinema/skor/fragments/GoalFragment.java
+++ app/src/main/java/id/ac/polinema/skor/fragments/GoalFragment.java
@@ -5,13 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import id.ac.polinema.skor.R;
+import id.ac.polinema.skor.databinding.FragmentGoalBinding;
 import id.ac.polinema.skor.models.GoalScorer;
+import id.ac.polinema.skor.fragments.ScoreFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class GoalFragment extends Fragment {
 
 	private String requestKey;
@@ -30,14 +32,21 @@ public class GoalFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		return null;
+		FragmentGoalBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_goal, container, false);
+		binding.setFragment(this);
+		binding.setGoalScorer(goalScorer);
+		requestKey = GoalFragmentArgs.fromBundle(getArguments()).getRequestKey();
+		return binding.getRoot();
 	}
 
 	public void onSaveClicked(View view) {
-
+		Bundle bundle = new Bundle();
+		bundle.putParcelable(ScoreFragment.SCORER_KEY, goalScorer);
+		getParentFragmentManager().setFragmentResult(requestKey, bundle);
+		Navigation.findNavController(view).navigateUp();
 	}
 
 	public void onCancelClicked(View view) {
-
+		Navigation.findNavController(view).navigateUp();
 	}
 }
--- app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
+++ app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
@@ -5,21 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.navigation.Navigation;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import id.ac.polinema.skor.R;
+import id.ac.polinema.skor.databinding.FragmentScoreBinding;
 import id.ac.polinema.skor.models.GoalScorer;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ScoreFragment extends Fragment {
 
 	public static final String HOME_REQUEST_KEY = "home";
 	public static final String AWAY_REQUEST_KEY = "away";
 	public static final String SCORER_KEY = "scorer";
+	private static Object GoalFragment;
 
 	private List<GoalScorer> homeGoalScorerList;
 	private List<GoalScorer> awayGoalScorerList;
@@ -34,19 +38,66 @@ public class ScoreFragment extends Fragment {
 		this.homeGoalScorerList = new ArrayList<>();
 		this.awayGoalScorerList = new ArrayList<>();
 	}
-
+	//menampung hasil data kembalian dari Fragment GoalFragment
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		return null;
+		FragmentScoreBinding binding = DataBindingUtil
+				.inflate(inflater, R.layout.fragment_score, container, false);
+		binding.setHomeGoalScorerList(homeGoalScorerList);
+		binding.setAwayGoalScorerList(awayGoalScorerList);
+		binding.setFragment(this);
+		View view = binding.getRoot();
+
+		getParentFragmentManager().setFragmentResultListener(HOME_REQUEST_KEY, this, new FragmentResultListener() {
+			@Override
+			public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+				GoalScorer goalScorer = result.getParcelable(SCORER_KEY);
+				homeGoalScorerList.add(goalScorer);
+			}
+		});
+
+		getParentFragmentManager().setFragmentResultListener(AWAY_REQUEST_KEY, this, new FragmentResultListener() {
+			@Override
+			public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+				GoalScorer goalScorer = result.getParcelable(SCORER_KEY);
+				awayGoalScorerList.add(goalScorer);
+			}
+		});
+		return view;
 	}
 
-	public void onAddHomeClick(View view) {
+	public String home(){
+		StringBuilder builder = new StringBuilder();
+		for (GoalScorer goalScorer : homeGoalScorerList){
+			builder.append(goalScorer.getName())
+					.append(" ")
+					.append(goalScorer.getMinute())
+					.append("\" ");
+		}
+		return builder.toString();
+	}
 
+	public String away(){
+		StringBuilder builder = new StringBuilder();
+		for (GoalScorer goalScorer : awayGoalScorerList){
+			builder.append(goalScorer.getName())
+					.append(" ")
+					.append(goalScorer.getMinute())
+					.append("\" ");
+		}
+		return builder.toString();
+	}
+
+	//berisi logika untuk berpindah ke GoalFragment. Pada perpindahan ini dikirimkan data tim home menggunakan konstanta HOME_REQUEST_KEY.
+	public void onAddHomeClick(View view) {
+		ScoreFragmentDirections.GoalScorerAction action = ScoreFragmentDirections.goalScorerAction(HOME_REQUEST_KEY);
+		Navigation.findNavController(view).navigate(action);
 	}
 
 	public void onAddAwayClick(View view) {
-
+		ScoreFragmentDirections.GoalScorerAction action = ScoreFragmentDirections.goalScorerAction(AWAY_REQUEST_KEY);
+		Navigation.findNavController(view).navigate(action);
 	}
 
 }
